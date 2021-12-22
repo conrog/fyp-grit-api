89a9aa6647df642c2f2fb70fe3c628337a2a9949
conrog <c17730535@mydit.ie>
2021-12-22 15:42:50 +0000

    probability now calculated using the scores of 3 nearest users

--- controllers/recomendationsController.js
+++ controllers/recomendationsController.js
@@ -9,7 +9,6 @@ exports.get_recommended_workouts = (req, res) => {
 
   db.manyOrNone("SELECT DISTINCT user_id FROM user_liked_workout")
     .then((data) => {
-      // console.log(data);
       // Create object lookup which servers as the user-item matrix
       for (let item of data) {
         matrix[item.user_id] = new Array();
@@ -18,24 +17,21 @@ exports.get_recommended_workouts = (req, res) => {
     })
     .then((matrix) => {
       db.manyOrNone("SELECT user_id, workout_id FROM user_liked_workout").then((data) => {
-        // console.log("data");
-        // console.log(data);
         // Populate the user-item matrix:
         // Key is the user_id and the value is an array of liked workout ids
         data.map((likedWorkout) => {
           matrix[likedWorkout.user_id].push(likedWorkout.workout_id);
         });
-        console.log("\n User-Item Matrix:");
+        console.log("\nUser-Item Matrix:");
         console.log(matrix);
 
-        // console.log(matrix[userId]);
         // Check if user has liked any workouts in order to generate recomendations
         if (matrix[userId] === undefined) {
           console.log("Error: User: " + userId + " has not liked any workouts ");
           res.send([]);
         } else {
           let user = matrix[userId];
-          console.log("User: " + userId + " likes workouts: " + user);
+          console.log("\nUser: " + userId + " likes workouts: " + user);
           // Calculate the similarity value between userId and all users in the user-item matrix
           // Finds the workouts for a given user that userId has not liked
           for (let key in matrix) {
@@ -51,24 +47,27 @@ exports.get_recommended_workouts = (req, res) => {
               workoutsNotLikedBySource: workoutsNotLikedBySource,
             });
           }
-          // console.log(similarities);
 
           // Sort function to sort by users most similar to userId
           similarities.sort((a, b) => {
             return b.score - a.score;
           });
 
-          // console.log(similarities);
           // Remove users that are not similar at all to userId (score = 0)
           let similarUsers = similarities.filter((similarity) => similarity.score != 0);
+          console.log("\nSimilar Users:");
           console.log(similarUsers);
 
-          //Implement KNN 3 nearest neighbours in terms of similarity
+          //Implement KNN where K = 3 nearest neighbours in terms of similarity
+          let K = 3;
+          let nearestNeighbours = similarUsers.splice(0, K);
+          console.log("\n3 Nearest Neighbours:");
+          console.log(nearestNeighbours);
 
           // Create object with keys equal to workout_ids with an array of similarity scores
           // value is an array of similarity scores of users who have liked the workout of the given key
-          // let likedWorkoutUsersSimilarityScore;
-          let likedWorkoutUsersSimilarityScore = similarUsers.reduce((recc, user) => {
+          // Used to calculate the probability of a user liking a workout
+          let likedWorkoutUsersSimilarityScore = nearestNeighbours.reduce((recc, user) => {
             user.workoutsNotLikedBySource.map((workout) => {
               if (recc.hasOwnProperty(workout)) {
                 recc[workout].push(user.score);
@@ -78,11 +77,9 @@ exports.get_recommended_workouts = (req, res) => {
             });
             return recc;
           }, {});
-          console.log("workout_id : [similarity score of users who liked workout_id]");
-          console.log(likedWorkoutUsersSimilarityScore);
 
           // Calculate Probability that a user will like a workout
-          // P(U, W) = sum of simalrity scores of users who like the workout W / amount of workout W has gotten
+          // P(U, W) = sum of simalrity scores of users who like the workout W / amount of likes workout W has recieved
           let probabilityOfLikingWorkout = {};
           for (var workout_id in likedWorkoutUsersSimilarityScore) {
             var sumOfSimilarity = likedWorkoutUsersSimilarityScore[workout_id].reduce((sum, val) => {
@@ -92,17 +89,18 @@ exports.get_recommended_workouts = (req, res) => {
             probabilityOfLikingWorkout[workout_id] =
               sumOfSimilarity / likedWorkoutUsersSimilarityScore[workout_id].length;
           }
+          console.log("\nProbability of liking Workouts: ");
           console.log(probabilityOfLikingWorkout);
 
           let reccomendedWorkoutIds = Object.keys(probabilityOfLikingWorkout);
-          // console.log(reccomendedWorkoutIds);
+
           if (reccomendedWorkoutIds.length == 0) {
             console.log("No recommendations");
           } else {
             let query = "SELECT * FROM workout WHERE workout_id IN (" + reccomendedWorkoutIds + ")";
-            // console.log(query);
             db.manyOrNone(query)
               .then((data) => {
+                console.log("\nRecommended Workouts: ");
                 console.log(data);
                 res.send(data);
               })
