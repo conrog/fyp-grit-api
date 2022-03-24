13b0a776314b5198a89a58ae6b085d13b398b6ea
conrog <c17730535@mydit.ie>
2022-03-24 13:47:10 +0000

    [GRIT-18] User Dashboard

--- controllers/workoutsController.js
+++ controllers/workoutsController.js
@@ -1,18 +1,6 @@
 const db = require("../postgres");
 const jwt_decode = require("jwt-decode");
 
-exports.get_workouts = (req, res) => {
-  db.manyOrNone("SELECT * FROM workout")
-    .then((data) => {
-      // console.log(data);
-      res.send(data);
-    })
-    .catch((error) => {
-      console.log("[GET /workouts] Error: " + error);
-    });
-};
-
-//Get user_workouts
 exports.get_user_workouts = async (req, res) => {
   try {
     const { userId } = jwt_decode(req.headers["authorization"].split(" ")[1]);
@@ -28,52 +16,45 @@ exports.get_user_workouts = async (req, res) => {
   }
 };
 
-exports.get_liked_workouts_by_user_id = (req, res) => {
-  db.manyOrNone(
-    "SELECT workout_id, workout_name FROM user_liked_workout JOIN workout USING (workout_id) WHERE user_liked_workout.user_id = $1",
-    req.params.userId
-  )
-    .then((data) => {
-      // console.log(data);
-      res.send(data);
-    })
-    .catch((error) => {
-      console.log("[GET /liked-workouts/:userId] Error: " + error);
-    });
+exports.get_liked_workouts = async (req, res) => {
+  try {
+    const { userId } = jwt_decode(req.headers["authorization"].split(" ")[1]);
+    let result = await db.manyOrNone(
+      "SELECT workout_id, workout_name FROM user_liked_workout JOIN workout USING (workout_id) WHERE user_liked_workout.user_id = $1",
+      [userId]
+    );
+    res.send(result);
+  } catch (error) {
+    console.log("[GET /liked-workouts/:userId] Error: " + error);
+  }
 };
 
-exports.like_workout = (req, res) => {
-  const { userId } = req.body;
-  const { workoutId } = req.params;
-
-  db.one(
-    "INSERT INTO user_liked_workout(user_id, workout_id) VALUES($1, $2) RETURNING user_id, workout_id",
-    [userId, workoutId]
-  )
-    .then((data) => {
-      res.status(201).send({ message: "Liked workout" });
-    })
-    .catch((error) => {
-      console.log("[POST /workout/:workoutId/like] Error: " + error);
-      res.send("Error");
-    });
+exports.like_workout = async (req, res) => {
+  try {
+    const { workoutId } = req.params;
+    const { userId } = jwt_decode(req.headers["authorization"].split(" ")[1]);
+    await db.one(
+      "INSERT INTO user_liked_workout(user_id, workout_id) VALUES($1, $2) RETURNING user_id, workout_id",
+      [userId, workoutId]
+    );
+    res.status(201).send({ message: "Liked workout" });
+  } catch (error) {
+    console.log("[POST /workout/:workoutId/like] Error: " + error);
+  }
 };
 
-exports.unlike_workout = (req, res) => {
-  const { userId } = req.body;
-  const { workoutId } = req.params;
-
-  db.oneOrNone(
-    "DELETE FROM user_liked_workout WHERE user_id = $1 AND workout_id = $2",
-    [userId, workoutId]
-  )
-    .then(() => {
-      res.status(200).send({ message: "Delete Successful" });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.send("Error");
-    });
+exports.unlike_workout = async (req, res) => {
+  try {
+    const { userId } = jwt_decode(req.headers["authorization"].split(" ")[1]);
+    const { workoutId } = req.params;
+    await db.oneOrNone(
+      "DELETE FROM user_liked_workout WHERE user_id = $1 AND workout_id = $2",
+      [userId, workoutId]
+    );
+    res.status(200).send({ message: "Delete Successful" });
+  } catch (error) {
+    console.log("[DELETE /workout/:workoutId/like] Error: " + error);
+  }
 };
 
 exports.create_workout = async (req, res) => {
