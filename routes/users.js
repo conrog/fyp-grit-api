8b8666d3c9c2a9dfd87c303cd533e211be224d0c
conrog <c17730535@mydit.ie>
2022-03-28 15:36:19 +0100

    [GRIT-13] Edit User Profile

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,10 @@ let router = express.Router();
 
 let userController = require("../controllers/usersController");
 
-router.get("/:user_name", userController.get_user_by_username);
-router.get("/", userController.get_users);
-router.post("/", userController.create_user);
+router
+  .get("/:user_name", userController.get_user_by_username)
+  .post("/:user_name", userController.update_user_information)
+  .get("/", userController.get_users)
+  .post("/", userController.create_user);
 
 module.exports = router;
